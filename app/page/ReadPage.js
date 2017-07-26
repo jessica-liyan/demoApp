6f3ec5d1185565622adaa664a04feceebce0050a
jessica-liyan <13125157002@163.com>
2017-07-26 18:53:58 +0800

    弹窗modal

--- app/page/ReadPage.js
+++ app/page/ReadPage.js
@@ -3,7 +3,9 @@ import {
   StyleSheet,
   View,
   Text,
-  ActivityIndicator
+  Modal,
+  ActivityIndicator,
+  TouchableHighlight
 } from 'react-native';
 import BaseComponent from '../base/BaseComponent'
 import {getNavigator} from '../route';
@@ -12,7 +14,8 @@ export default class ReadPage extends BaseComponent {
   constructor (props) {
     super(props)
     this.state = {
-      type: 'read'
+      type: 'read',
+      modalVisible: false
     }
   }
 
@@ -25,11 +28,35 @@ export default class ReadPage extends BaseComponent {
     };
   }
 
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   renderBody () {
     return (
       <View>
         <Text>read books</Text>
         <ActivityIndicator color={'red'}/>
+        <TouchableHighlight onPress={() => {this.setModalVisible(true)}}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+        <Modal
+          animationType={"fade"}
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+         <View style={{flex:1,alignItems:'center',justifyContent: 'center',backgroundColor:'rgba(0, 0, 0, 0.5)'}}>
+          <View style={{width:200,height:200,backgroundColor:'#fff',padding:20,borderRadius:8}}>
+            <Text>Hello World!</Text>
+            <TouchableHighlight onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+         </View>
+        </Modal>
       </View>
     )
   }
